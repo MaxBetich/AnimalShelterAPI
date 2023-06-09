bddf53806dbc0478901d1c78548456e5265d49cb
Max Betich <betichm@gmail.com>
2023-06-09 14:06:33 -0700

    begin implementing versioning support

--- AnimalShelter/Program.cs
+++ AnimalShelter/Program.cs
@@ -1,5 +1,7 @@
 using AnimalShelter.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,17 +17,40 @@ builder.Services.AddDbContext<AnimalShelterContext>(
                     )
                   )
                 );
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(opt =>
+                    {
+                      opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                      opt.AssumeDefaultVersionWhenUnspecified = true;
+                      opt.ReportApiVersions = true;
+                      opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                      new HeaderApiVersionReader("x-api-version"),
+                                                                      new MediaTypeApiVersionReader("x-api-version"));
+                    });
 
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+  setup.GroupNameFormat = "'v'VVV";
+  setup.SubstituteApiVersionInUrl = true;
+});
 var app = builder.Build();
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI(options =>
+  {
+    foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+    {
+      options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+          description.GroupName.ToUpperInvariant());
+    }
+  });
 }
 else
 {
